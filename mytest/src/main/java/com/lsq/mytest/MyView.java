2d982d384d289273082f330a11c9a666ffcee6eb
Tame-complexity <linshengqian@outlook.com>
2021-12-23 14:59:43 +0800

    android

--- mytest/src/main/java/com/lsq/mytest/MyView.java
+++ mytest/src/main/java/com/lsq/mytest/MyView.java
@@ -3,6 +3,8 @@ package com.lsq.mytest;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.view.View;
 
 /**
@@ -17,8 +19,28 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setColor(0xFFBB86FC);
-        paint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(10,10,1000,1000,paint);
+        paint.setAntiAlias(true);   //抗锯齿
+        paint.setColor(0xFFA47739);
+
+        //head
+
+        RectF rectF = new RectF(10,10,100,100);
+        rectF.offset(90,20);
+        canvas.drawArc(rectF ,-10,-180,false,paint);
+        //eye
+        paint.setColor(0xFFFFFFFF);
+        canvas.drawCircle(165,53,4,paint);
+        canvas.drawCircle(125,53,4,paint);
+        //wifi
+        paint.setStrokeWidth(2);
+        paint.setColor(0xFFA4C730);
+        canvas.drawLine(100,15,125,35,paint);
+        canvas.drawLine(180,15,165,35,paint);
+
+
+
+
+
+
     }
 }
